1d8fd6c83c4351ddd38e121706418329511ffea3
Artur Medvedev <varouser@gmail.com>
2024-03-18 00:48:57 +0500

    Add dotenv

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,8 +1,15 @@
 from flask import Flask, render_template
+import psycopg2
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+DATABASE_URL = os.getenv('DATABASE_URL')
+conn = psycopg2.connect(DATABASE_URL)
 
 
 @app.route("/")
-def hello_world():
+def index():
     return render_template('index.html')
