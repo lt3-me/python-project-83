d64af24bd5c9581835503a5d75370559ea47f15b
Artur Medvedev <varouser@gmail.com>
2024-03-23 08:45:39 +0500

    Fix 422 response code

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -22,7 +22,7 @@ def urls_post():
     url = request.form.get('url')
     if not validate_url(url):
         flash('Некорректный URL', 'error')
-        return redirect(url_for('index'), 422)
+        return render_template('index.html'), 422
 
     parsed_url = urlparse(url)
     url = parsed_url.scheme + '://' + parsed_url.netloc
