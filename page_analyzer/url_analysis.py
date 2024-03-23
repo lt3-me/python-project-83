569b8908781820397b529064c17200059ab8618d
Artur Medvedev <varouser@gmail.com>
2024-03-23 08:04:29 +0500

    Handled ConnectionError

--- page_analyzer/url_analysis.py
+++ page_analyzer/url_analysis.py
@@ -3,8 +3,8 @@ from bs4 import BeautifulSoup
 
 
 def check_url(url):
-    r = requests.get(url=url)
     try:
+        r = requests.get(url=url)
         r.raise_for_status()
         status_code = r.status_code
         html_content = r.text
@@ -12,6 +12,8 @@ def check_url(url):
         return {'status_code': status_code, 'h1': h1,
                 'title': title, 'description': desc}
     except requests.exceptions.HTTPError:
+        return None
+    except requests.exceptions.ConnectionError:
         return None
 
 
